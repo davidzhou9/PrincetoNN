b7292c5702cd2be3bbf62da30b9e79ca949d7997
Jad Bechara <jad@nat-oitwireless-inside-vapornet100-b-24785.Princeton.EDU>
2017-04-01 16:47:08 -0400

    DiningNew

--- src/CallAPIs.js
+++ src/CallAPIs.js
@@ -2,54 +2,15 @@ var https = require('https');
 
 module.exports = {
 
-    getPopFromAPI_POST: function(myState, callback) {
-
-        var population = 0;
-        var rank = 0;
-
-        var post_data = {"usstate": myState};
-
-        var post_options = {
-            host:  'cp6gckjt97.execute-api.us-east-1.amazonaws.com',
-            port: '443',
-            path: '/prod/stateresource',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(JSON.stringify(post_data))
-            }
-        };
-
-        var post_req = https.request(post_options, res => {
-            res.setEncoding('utf8');
-            var returnData = "";
-            res.on('data', chunk =>  {
-                returnData += chunk;
-            });
-            res.on('end', () => {
-                // this API returns a JSON structure
-
-                population = JSON.parse(returnData).population;
-
-                callback(population);
-
-            });
-        });
-        post_req.write(JSON.stringify(post_data));
-        post_req.end();
-
-
-    },
-
     getDiningFromAPI_GET: (resCollege, mealTime, callback) => {
 
         var population = 0;
         var rank = 0;
 
         var options = {
-            host: 'galstyan.net',
-            port: 80,
-            path: '/haha.txt',// + encodeURI(myState),
+            host: 'cp6gckjt97.execute-api.us-east-1.amazonaws.com',
+            port: 443,
+            path: '/prod/stateresource?usstate=' + encodeURI('texas'),
             method: 'GET'
         };
         console.log("options");
@@ -67,7 +28,7 @@ module.exports = {
 
                 console.log(JSON.stringify(returnData));
 
-                answer = returnData;
+                answer = JSON.parse(returnData).population;
 
                 callback(answer);
 
@@ -78,14 +39,7 @@ module.exports = {
         req.end();
 
 
-    },
-
-    RandomPhrase: function (listOfPhrases, callback) {
-
-        var i = 0;
-        i = Math.floor(Math.random() * listOfPhrases.length);
-        callback(listOfPhrases [i]);
-
     }
+
 };
 
