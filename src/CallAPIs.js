9703c1e6b97fe527cf6934afeaeed51bd2ea1623
Jad Bechara <jad@nat-oitwireless-inside-vapornet100-b-24785.Princeton.EDU>
2017-04-02 06:21:06 -0400

    almost fixed invalid timestamp

--- src/CallAPIs.js
+++ src/CallAPIs.js
@@ -9,7 +9,17 @@ exports = {
  piazza_whatCourses:piazza.piazza_whatCourses,
  piazza_whichInstructors:piazza.piazza_whichInstructors,
   
- tigerbook_random:tigerbook.tigerbook_random
+ tigerbook_random:tigerbook.tigerbook_random,
+ tigerbook_college:tigerbook.tigerbook_college,
+ tigerbook_room:tigerbook.tigerbook_room,
+ tigerbook_hall:tigerbook.tigerbook_hall,
+ tigerbook_program:tigerbook.tigerbook_program,
+ tigerbook_major:tigerbook.tigerbook_major,
+ tigerbook_locate:tigerbook.tigerbook_locate,
+ tigerbook_year:tigerbook.tigerbook_year,
+ tigerbook_mailbox:tigerbook.tigerbook_mailbox,
+ tigerbook_email:tigerbook.tigerbook_email,
+ tigerbook_roommates:tigerbook.tigerbook_roommates
 
 };
 
--- src/tigerbook.js
+++ src/tigerbook.js
@@ -3,11 +3,18 @@ var https = require('https');
 var request = require('request');
 var wsse = require('wsse');
 
+var bible = null;
+
 var tigerbook = {};
     
-tigerbook.tigerbook_random =  (callback) => {
-
-var token = wsse({ username: 'jbechara+testApp', password: '1b58c3830f28cd21f9b8abd9f8207de5' });
+tigerbook.tigerbook_random = (callback) => {
+var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+console.log(token + '');
+if (bible) {
+  var names = JSON.parse(body).map((person)=>[person.full_name, person.net_id]);
+  var rand = Math.floor(Math.random() * names.length);
+  callback([names[rand][0], names[rand][1]]);
+} else {
 request({
     headers: {
       'Authorization': 'WSSE Profile="UsernameToken"',
@@ -16,9 +23,204 @@ request({
     uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates',
     method: 'GET'
     }, function (err, res, body) {
-        var names = JSON.parse(body).map((person)=>person.full_name);
-        callback(names[Math.floor(Math.random() * names.length)]);
+        bible = JSON.parse(body);
+        var names = JSON.parse(body).map((person)=>[person.full_name, person.net_id]);
+        var rand = Math.floor(Math.random() * names.length);
+        callback([names[rand][0], names[rand][1]]);
+    });
+}
+}
+
+tigerbook.tigerbook_college = (student, callback) => {
+  if (bible) {
+    var college = bible.filter((person) => person.net_id == student)[0]['res_college'];
+    callback(college);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['res_college']);
+    });
+}
+}
+
+tigerbook.tigerbook_room = (student, callback) => {
+  if (bible) {
+    var room = bible.filter((person) => person.net_id == student)[0]['dorm_number'];
+    callback(room);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['dorm_number']);
+    });
+}
+}
+
+tigerbook.tigerbook_hall = (student, callback) => {
+  if (bible) {
+    var hall = bible.filter((person) => person.net_id == student)[0]['dorm_building'];
+    callback(hall);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['dorm_building']);
+    });
+}
+}
+
+tigerbook.tigerbook_program = (student, callback) => {
+  if (bible) {
+    var majorType = bible.filter((person) => person.net_id == student)[0]['major_type'];
+    callback(majorType);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['major_type']);
+    });
+}
+}
+
+tigerbook.tigerbook_major = (student, callback) => {
+  if (bible) {
+    var major = bible.filter((person) => person.net_id == student)[0]['major_raw'];
+    callback(major);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['major_raw']);
+    });
+}
+}
+
+tigerbook.tigerbook_locate = (student, callback) => {
+  if (bible) {
+    var hometown = bible.filter((person) => person.net_id == student)[0]['hometown'];
+    callback(hometown);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['hometown']);
+    });
+}
+}
+
+tigerbook.tigerbook_year = (student, callback) => {
+  if (bible) {
+    var year = bible.filter((person) => person.net_id == student)[0]['class_year'];
+    callback(year);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['class_year']);
+    });
+}
+}
+
+tigerbook.tigerbook_mailbox = (student, callback) => {
+  if (bible) {
+    var mailbox = bible.filter((person) => person.net_id == student)[0]['mailbox'];
+    callback(mailbox);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['mailbox']);
+    });
+}
+}
+
+tigerbook.tigerbook_email = (student, callback) => {
+  if (bible) {
+    var email = bible.filter((person) => person.net_id == student)[0]['email'];
+    callback(email);
+  } else {
+	var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+request({
+    headers: {
+      'Authorization': 'WSSE Profile="UsernameToken"',
+      'X-WSSE': token + ''
+    },
+    uri: 'https://tigerbook.herokuapp.com/api/v1/undergraduates/' + encodeURI(student),
+    method: 'GET'
+    }, function (err, res, body) {
+        callback(JSON.parse(body)['email']);
+    });
+}
+}
+
+tigerbook.tigerbook_roommates = (student, callback) => {
+  var roommates = [];
+  var token = wsse({ username: 'jbechara+testApp', password: '532d5717a37f0e4abe8aa5e36f7a98a5' });
+  request({
+    headers: {
+        'Authorization': 'WSSE Profile="UsernameToken"',
+        'X-WSSE': token + ''
+      },
+      uri: 'https://tigerbook.herokuapp.com/api/v1/roommates/' + encodeURI(student),
+      method: 'GET'
+    }, function (err, res, body) {
+          roommates = JSON.parse(body)['roommates'];
+          var result = [];
+          for (var i = 0; i < roommates.length; i++) {
+            result[i] = bible.filter((person) => person.net_id == roommates[i])[0]['full_name'];
+          }
+          callback(result);
     });
 }
 
 module.exports = tigerbook;
+tigerbook.tigerbook_random(console.log);
