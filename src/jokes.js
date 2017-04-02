aa7322e5ea3df324efb48f82e3764898d165adeb
Narek Galstyan <ngalstyan4@gmail.com>
2017-04-02 09:15:18 -0400

    added jokes

--- src/jokes.js
+++ src/jokes.js
@@ -15,6 +15,12 @@ jokes.jokes_whatJokes = (callback) => {
         "How many Princeton students does it take to change a light bulb? " +
         "Two. One to mix the martinis and one to call the electrician.",
 
+        "A young man hired by a supermarket reported for his first day of work." +
+        "The manager greeted him with a warm handshake and a smile, gave him a broom and said," +
+        "Your first job will be to sweep out the store." +
+        "But I'm a harvard graduate, the young man replied indignantly." +
+        "Oh, I'm sorry. I didn't know that, said the manager. Here, give me the broom -- I'll show you how.",
+
         "How many Harvard students does it take to change a lightbulb? " +
         "One, he holds the bulb and the world revolves around him",
 
@@ -28,7 +34,11 @@ jokes.jokes_whatJokes = (callback) => {
             "None, New Haven looks better in the dark.",
 
         "All Dartmouth students are bilingual. " +
-            "They speak English and profanity."
+            "They speak English and profanity.",
+
+        "How many Columbia students does it take to change a lightbulb? Seventy-six --" +
+        " one to change the lightbulb, fifty to protest the lightbulb's right to not change," +
+        "and twenty-five to hold a counter-protest."
     ]
 
     callback(jokesArray[Math.floor(Math.random() * jokesArray.length)]);
